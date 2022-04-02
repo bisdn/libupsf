a50bf679f2c6ee7b5b85f052430d8038bbcae473
Andreas Koepsel <andreas.koepsel@bisdn.de>
2022-04-02 08:12:34 +0200

    initial commit

--- upsfcli/upsf_client.cpp
+++ upsfcli/upsf_client.cpp
@@ -6,12 +6,17 @@ int main(int argc, char** argv) {
       grpc::CreateChannel("localhost:50051",
                           grpc::InsecureChannelCredentials()));
 
-  bbf::sss::UpdateServiceGatewayReq req;
   bbf::sss::ServiceGatewayResp resp;
+  bbf::sss::UpdateServiceGatewayReq req;
+
+  req.mutable_service_gateway()->mutable_metadata()->set_name(std::string("up1"));
 
   if (!client.UpdateServiceGateway(req, resp)) {
     std::cerr << "failed to call UpdateServiceGateway" << std::endl;
   }
+
+  std::cout << resp.result() << std::endl;
+  std::cout << resp.service_gateway().id() << std::endl;
 
   return 0;
 };
